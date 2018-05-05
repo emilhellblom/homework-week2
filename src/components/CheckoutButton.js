dbbaeebe2fd7286ed9b101fbc1ac4b0681d9819c
emilhellblom <ehellblom@outlook.com>
2018-05-05 20:21:37 +0200

    Final commit

--- src/components/CheckoutButton.js
+++ src/components/CheckoutButton.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import './CheckoutButton.css';
 
 export const CheckoutButton = ({showCheckout}) => (
   <button className='checkout-button' onClick={() => showCheckout()}>Checkout</button>
 );
+
+CheckoutButton.propTypes = {
+  showCheckout: PropTypes.func.isRequired,
+}
